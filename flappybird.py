49788d463908feb0176f187506c9c6cc8b2ae662
urmishukla <63452810+urmishukla@users.noreply.github.com>
2020-05-23 16:14:25 +0530

    added regularly occurring pipes
    added green graphic to pipes. changed background. pipe pattern is not yet randomized.

--- flappybird.py
+++ flappybird.py
@@ -1,13 +1,17 @@
 import turtle
 import time
+import random
 
 wn= turtle.Screen()
 wn.title("Flappy Bird by @urmishukla")
-wn.bgcolor("deepskyblue")
+#wn.bgcolor("deepskyblue")
 wn.setup(width=500,height=800)
+wn.bgpic("bgFadedFinalGIF.gif")
 wn.tracer(0)
 
 wn.register_shape("smallBirdGif.gif")
+wn.register_shape("3x30Pipe.gif")
+
 
 player=turtle.Turtle()
 
@@ -19,30 +23,31 @@ player.goto(-200,0)
 player.dx=0
 player.dy=1
 
-pipe1_top= turtle.Turtle()
-pipe1_top.speed(0)
-pipe1_top.penup()
-pipe1_top.color("forestgreen")
-pipe1_top.shape("square")
-pipe1_top.shapesize(stretch_wid =18, stretch_len = 3, outline =None)
-pipe1_top.goto (0,250)
-pipe1_top.dx = -2
-pipe1_top.dy= 0
 
-pipe1_bottom= turtle.Turtle()
-pipe1_bottom.speed(0)
-pipe1_bottom.penup()
-pipe1_bottom.color("forestgreen")
-pipe1_bottom.shape("square")
-pipe1_bottom.shapesize(stretch_wid =18, stretch_len = 3, outline =None)
-pipe1_bottom.goto (0,-250)
-pipe1_bottom.dx = -2
-pipe1_bottom.dy= 0
+class pipes:
+    limit = random.randint(0, 500)
+    pipe_top= turtle.Turtle()
+    pipe_top.speed(0)
+    pipe_top.penup()
+    #pipe_top.color("blue")
+    pipe_top.shape("3x30Pipe.gif")
+    pipe_top.shapesize(stretch_wid =30, stretch_len = 3, outline =None)
+    pipe_top.goto (200, (250+limit))
+    pipe_top.dx = -2
+    pipe_top.dy= 0
 
+    pipe_bottom= turtle.Turtle()
+    pipe_bottom.speed(0)
+    pipe_bottom.penup()
+    #pipe_bottom.color("forestgreen")
+    pipe_bottom.shape("3x30Pipe.gif")
+    pipe_bottom.shapesize(stretch_wid =30, stretch_len = 3, outline =None)
+    pipe_bottom.goto (200, (limit-500))
+    pipe_bottom.dx = -2
+    pipe_bottom.dy= 0
+  
 
-
-
-gravity = -0.25
+gravity = -0.4
 
 def goUp():
     player.dy += 7.5
@@ -50,8 +55,11 @@ def goUp():
 wn.listen()
 wn.onkeypress(goUp, "space")
     
+p1=pipes()
 
+  
 #main game loop
+roundsWon=0
 while True:
     #pause
     time.sleep(0.02)
@@ -63,17 +71,34 @@ while True:
     y = player.ycor()
     y += player.dy
     player.sety(y)
+    #if player.ycor() < -380:
+        #player.dy = 0
+        #player.sety(-390)
     
     #move pipes
-    x= pipe1_top.xcor()
-    x += pipe1_top.dx
-    pipe1_top.setx(x)
+    x= p1.pipe_top.xcor()
+    x += p1.pipe_top.dx
+    p1.pipe_top.setx(x)
     
-    x= pipe1_bottom.xcor()
-    x += pipe1_bottom.dx
-    pipe1_bottom.setx(x)
-
+    x= p1.pipe_bottom.xcor()
+    x += p1.pipe_bottom.dx
+    p1.pipe_bottom.setx(x)
+    
+    if (-180.0 == p1.pipe_top.xcor() and player.ycor()>= p1.pipe_top.ycor() -230.0):
+        #print((p1.pipe_top.xcor()), (p1.pipe_top.ycor()-225), player.ycor())
+        wn.bye()
+    elif (-180.0 == p1.pipe_bottom.xcor() and player.ycor() <= p1.pipe_bottom.ycor()+225.0):
+        #print((p1.pipe_top.xcor()), (p1.pipe_top.ycor()-225), player.ycor())
+        wn.bye()
+    if p1.pipe_top.xcor() < -300:
+        limit = random.randint(0, 350)
+        p1.pipe_top.setx(200)
+        p1.pipe_top.sety(250+limit)
+        p1.pipe_bottom.setx(200)
+        p1.pipe_bottom.sety(limit-500)
+    
 
 
     
 wn.mainloop()
+
